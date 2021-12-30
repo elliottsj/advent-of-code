f6afd57c63a7229384eef0733280e0f1042e7a72
Spencer Elliott <me@elliottsj.com>
2021-12-30 16:59:48 -0500

    Day 1 part 1

--- 2021/day01.py
+++ 2021/day01.py
@@ -10,4 +10,14 @@ path = Path(__file__).parent / "day01input.txt"
 
 with path.open() as f:
     depths = [int(num.strip()) for num in f.readlines()]
-    print(depths)
+
+increases = 0
+last = 0
+for depth in depths:
+    if last == 0:
+        last = depth
+    elif depth > last:
+        increases += 1
+    last = depth
+
+print(increases)
